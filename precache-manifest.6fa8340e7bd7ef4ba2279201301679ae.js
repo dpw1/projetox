c6ad64cd9ad49d5d7b194cbd895206354d0296a7
Diego Fortes <diego.boarutto.fortes@gmail.com>
2019-11-12 03:39:59 +0000

    Updates

---
+++ precache-manifest.6fa8340e7bd7ef4ba2279201301679ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89e6307662f8787cebf2c8ff462831aa",
+    "url": "/projetox/index.html"
+  },
+  {
+    "revision": "8d308ad6bf3114099636",
+    "url": "/projetox/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "09ce0c75260fdeee0801",
+    "url": "/projetox/static/js/2.60901ae9.chunk.js"
+  },
+  {
+    "revision": "8d308ad6bf3114099636",
+    "url": "/projetox/static/js/main.108a5190.chunk.js"
+  },
+  {
+    "revision": "2a4b94d15f11dbd6e5a9",
+    "url": "/projetox/static/js/runtime-main.910e861f.js"
+  }
+]);
