e6211d9bd4ddf175b0f8650e03053aef09c9c9bf
Diego Fortes <diego.boarutto.fortes@gmail.com>
2019-11-12 04:15:26 +0000

    [UPD] github page config

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
+import {
+  HashRouter,
+  BrowserRouter,
+  Route,
+  Switch,
+  Link,
+} from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import Dashboard from "./dashboard/Dashboard";
