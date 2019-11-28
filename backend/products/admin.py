4f8cee418d94b4fe542149593400b9a1771b9bfe
Diego Fortes <diego.boarutto.fortes@gmail.com>
2019-11-27 23:28:27 -0300

    [ADD] adding products via the Admin

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product
+from variations.models import Variation
 
 
-admin.site.register(Product)
+class VariationInline(admin.StackedInline):
+    model = Variation
+    extra = 1
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        VariationInline,
+    ]
+
+    def get_changeform_initial_data(self, request):
+        return {'name': 'as'}
+
+
+admin.site.register(Product, ProductAdmin)
--- backend/products/models.py
+++ backend/products/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.conf import settings
 
+# NOTE: the value "1" may vary. It should be set to the Admin's ID.
+CREATED_BY_DEFAULT = 1
 # Create your models here.
 
 
 class Product(models.Model):
     created_by = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+        settings.AUTH_USER_MODEL,  default=CREATED_BY_DEFAULT, on_delete=models.CASCADE)
     name = models.CharField(max_length=300)
     description = models.TextField()
     category = models.CharField(max_length=15)
